9fb19912a02ad428e4103d9c337211b9ccae357c
Geralda Livia Nurgaha <93833681+geraldalivia@users.noreply.github.com>
2025-03-19 19:14:57 +0700

    Update air-quality-project.py

--- Dashboard/air-quality-project.py
+++ Dashboard/air-quality-project.py
@@ -105,9 +105,9 @@ if options == "Daily PM10 Pattern":
 
     st.info("""
     **Insights about PM10 Daily Patterns:**\n
-            - Both stations show a pattern with two peaks. One in the morning (around 8-9 AM) and another in the evening (around 9-10 PM)\n
-            - The evening peak was more pronounced, with PM10 levels reaching a maximum around 21:00 (9 PM), reaching values of around 130 at both stations. However, the lowest concentrations were recorded during the day between 13:00-15:00 (1-3 PM)\n
-            - Dongsi and Wanliu stations show almost identical patterns, indicating that this phenomenon is regional and not just related to a particular location\n
+    - Both stations show a pattern with two peaks. One in the morning (around 8-9 AM) and another in the evening (around 9-10 PM)\n
+    - The evening peak was more pronounced, with PM10 levels reaching a maximum around 21:00 (9 PM), reaching values of around 130 at both stations. However, the lowest concentrations were recorded during the day between 13:00-15:00 (1-3 PM)\n
+    - Dongsi and Wanliu stations show almost identical patterns, indicating that this phenomenon is regional and not just related to a particular location\n
     """)
 
     # Monthly average PM10
@@ -124,10 +124,10 @@ if options == "Daily PM10 Pattern":
 
     st.info("""
     **Insights about PM10 Daily Patterns:**\n
-            - Seasonal variation with highest concentrations in winter/spring (around 3rd month or March) and lowest in summer (around 8th month or August)\n
-            - March shows peak concentrations (>140) at both stations, likely due to a combination of warming emissions and meteorological factors\n
-            - The summer months (around 6th to 9th month or June-September) consistently show lower PM10 levels (<90), likely due to increased rainfall\n
-            - Both stations show similar seasonal patterns, confirming the regional nature of air pollution\n
+    - Seasonal variation with highest concentrations in winter/spring (around 3rd month or March) and lowest in summer (around 8th month or August)\n
+    - March shows peak concentrations (>140) at both stations, likely due to a combination of warming emissions and meteorological factors\n
+    - The summer months (around 6th to 9th month or June-September) consistently show lower PM10 levels (<90), likely due to increased rainfall\n
+    - Both stations show similar seasonal patterns, confirming the regional nature of air pollution\n
     """)
 
 
@@ -147,8 +147,8 @@ elif options == "Weather Effects on PM10":
 
     st.info("""
     **Insights about PM10 Daily Patterns:**\n
-            - Temperature (TEMP) and Dew Point (DEWP) showed a negative correlation at both stations. For TEMP it is about -0.27 at Dongsi and -0.22 at Wanliu. For DEWP it is about -0.055 at Dongsi and -0.028 at Wanliu\n
-            - Air Pressure (PRES) shows a weak positive correlation at both stations. About 0.074 at Dongsi and 0.029 at Wanliu\n
+    - Temperature (TEMP) and Dew Point (DEWP) showed a negative correlation at both stations. For TEMP it is about -0.27 at Dongsi and -0.22 at Wanliu. For DEWP it is about -0.055 at Dongsi and -0.028 at Wanliu\n
+    - Air Pressure (PRES) shows a weak positive correlation at both stations. About 0.074 at Dongsi and 0.029 at Wanliu\n
     """)
 
     # Scatter plots for interactive analysis
@@ -177,9 +177,9 @@ elif options == "Weather Effects on PM10":
 
     st.info("""
     **Insights about PM10 Daily Patterns:**\n
-            - It is noted on the scatter plot of PM10 concentration and TEMP that PM10 concentration tends to increase when the temperature is below 0°C. In line with Temperature, Dew point (DEWP) also follows a similar pattern but with more scatter in its relationship\n
-            - The air pressure (PRES) relationship is less clear, but there is a tendency for higher PM10 during high pressure systems\n
-            - The similar patterns at both stations suggest that meteorological factors affect the entire region in the same way\n
+    - It is noted on the scatter plot of PM10 concentration and TEMP that PM10 concentration tends to increase when the temperature is below 0°C. In line with Temperature, Dew point (DEWP) also follows a similar pattern but with more scatter in its relationship\n
+    - The air pressure (PRES) relationship is less clear, but there is a tendency for higher PM10 during high pressure systems\n
+    - The similar patterns at both stations suggest that meteorological factors affect the entire region in the same way\n
     """)
 
 # Further Analysis
@@ -213,11 +213,11 @@ elif options == "Further Analysis":
 
     st.info("""
     **Insights about PM10 Daily Patterns:**\n
-            - All temperature categories show similar patterns with peaks in the evening/night (around 6-10 PM) and troughs during the day (around 12-3 PM). This shows that apart from temperature, other factors such as daily human activities and atmospheric conditions affect PM10 concentrations\n
-            - During the day, both stations with a temperature category of “Cold” showed a great decline around noon (around 12-3 PM), which formed a typical V-shaped pattern. This may indicate that even in cold weather, warmer daytime temperatures can temporarily improve air quality\n
-            - The highest PM10 concentrations occur at night with a temperature category of “Cold” around 200 in Dongsi and 160 in Wanliu. This suggests that cold nighttime conditions create the worst air quality scenario\n
-            - Both stations show similar patterns. However, Dongsi generally has higher PM10 concentrations under temperature conditions with the “Cold” category compared to Wanliu indicating possible site-specific factors affecting pollution dispersion\n
-            - In the “Warm” temperature category, PM10 shows a stable graph throughout the day, with much less variation compared to the other temperature categories, suggesting that warmer conditions may result in more consistent air quality\n
+    - All temperature categories show similar patterns with peaks in the evening/night (around 6-10 PM) and troughs during the day (around 12-3 PM). This shows that apart from temperature, other factors such as daily human activities and atmospheric conditions affect PM10 concentrations\n
+    - During the day, both stations with a temperature category of “Cold” showed a great decline around noon (around 12-3 PM), which formed a typical V-shaped pattern. This may indicate that even in cold weather, warmer daytime temperatures can temporarily improve air quality\n
+    - The highest PM10 concentrations occur at night with a temperature category of “Cold” around 200 in Dongsi and 160 in Wanliu. This suggests that cold nighttime conditions create the worst air quality scenario\n
+    - Both stations show similar patterns. However, Dongsi generally has higher PM10 concentrations under temperature conditions with the “Cold” category compared to Wanliu indicating possible site-specific factors affecting pollution dispersion\n
+    - In the “Warm” temperature category, PM10 shows a stable graph throughout the day, with much less variation compared to the other temperature categories, suggesting that warmer conditions may result in more consistent air quality\n
     """)
 
 # Conclusion
@@ -227,23 +227,23 @@ elif options == "Conclusion":
     **PM10 Daily Patterns**\n
     ****
     **Findings:**\n
-            - Highest Point: Occurred at 21:00 (9 PM) with values of 130.3 in Dongsi and 129.7 in Wanliu\n
-            - Lowest Point: Occurred at noon around 13:00-14:00 (1-2 PM) with values of 95.3 in Dongsi and 94.2 in Wanliu\n
-            - Daily pattern: There are two peaks in a day (bi-modal pattern) - morning around 8-9 AM and evening around 21-22 PM\n
+    - Highest Point: Occurred at 21:00 (9 PM) with values of 130.3 in Dongsi and 129.7 in Wanliu\n
+    - Lowest Point: Occurred at noon around 13:00-14:00 (1-2 PM) with values of 95.3 in Dongsi and 94.2 in Wanliu\n
+    - Daily pattern: There are two peaks in a day (bi-modal pattern) - morning around 8-9 AM and evening around 21-22 PM\n
     **Reason:**\n
-            - The morning peak coincides with the morning peak of transportation and industrial activities\n
-            - The evening peak is due to a combination of afternoon/evening transportation activities and more stable atmospheric layer conditions at night (temperature inversion)\n
-            - The low PM10 concentration during the day is likely due to the increased height of the atmospheric mixing layer and thermal turbulence that aids pollutant dispersion\n
+    - The morning peak coincides with the morning peak of transportation and industrial activities\n
+    - The evening peak is due to a combination of afternoon/evening transportation activities and more stable atmospheric layer conditions at night (temperature inversion)\n
+    - The low PM10 concentration during the day is likely due to the increased height of the atmospheric mixing layer and thermal turbulence that aids pollutant dispersion\n
     
     **Seasonal Patterns of PM10 Concentrations (2013-2017)**\n
     ****
     **Findings:**\n
-            - Month with Highest PM10: March with values of 142.6 in Dongsi and 146.3 in Wanliu\n
-            - Month with Lowest PM10: August with values of 73.8 in Dongsi and 78.2 in Wanliu\n
-            - Seasonal Variations: Winter/semi-season (December-March) shows 30-50% higher PM10 concentrations than summer (June-August)\n
+    - Month with Highest PM10: March with values of 142.6 in Dongsi and 146.3 in Wanliu\n
+    - Month with Lowest PM10: August with values of 73.8 in Dongsi and 78.2 in Wanliu\n
+    - Seasonal Variations: Winter/semi-season (December-March) shows 30-50% higher PM10 concentrations than summer (June-August)\n
     **Reason:**\n
-            - High PM10 concentrations in March are related to meteorological conditions\n
-            - The low concentrations in summer (June-August) are attributed to heavy rainfall which helps to remove pollutants from the air (wet deposition)\n
+    - High PM10 concentrations in March are related to meteorological conditions\n
+    - The low concentrations in summer (June-August) are attributed to heavy rainfall which helps to remove pollutants from the air (wet deposition)\n
     """)
     
     st.title("Conclusion Question 2 [What are the specific effects of temperature (TEMP), dew point(DEWP), and air pressure (PRES) on PM10 at Dongsi and Wanliu stations over the past 12 months?]")
@@ -251,46 +251,46 @@ elif options == "Conclusion":
     **Effects of Weather Parameters on PM10 (Last 12 Months)**\n
     ****
     **Findings:**\n
-            1. Temperature (TEMP)\n
-                - Negative Correlation: -0.27 in  Dongsi and -0.22 in Wanliu\n
-                - Highest PM10: Occurs when temperatures are below 0°C, reaching >150 in extremely cold conditions\n
-                - PM10 Lows: Occurs when temperatures are 10-20°C (mild), with values around 70\n
-            2. Dew Point (DEWP)\n
-                - Weak Negative Correlation: -0.055 in Dongsi and -0.028 in Wanliu\n
-                - Pattern: Similar to temperature but with a weaker correlation\n
-            3. Air Pressure (PRES)\n
-                - Weak Positive Correlation: 0.074 in Dongsi and 0.029 in Wanliu\n
-                - Highest PM10: Seen in high pressure systems with values of 143.9 in Dongsi and 127.8 in Wanliu\n
+    1. Temperature (TEMP)\n
+        - Negative Correlation: -0.27 in  Dongsi and -0.22 in Wanliu\n
+        - Highest PM10: Occurs when temperatures are below 0°C, reaching >150 in extremely cold conditions\n
+        - PM10 Lows: Occurs when temperatures are 10-20°C (mild), with values around 70\n
+    2. Dew Point (DEWP)\n
+        - Weak Negative Correlation: -0.055 in Dongsi and -0.028 in Wanliu\n
+        - Pattern: Similar to temperature but with a weaker correlation\n
+    3. Air Pressure (PRES)\n
+        - Weak Positive Correlation: 0.074 in Dongsi and 0.029 in Wanliu\n
+        - Highest PM10: Seen in high pressure systems with values of 143.9 in Dongsi and 127.8 in Wanliu\n
     **Reason:**\n
-            - Low temperatures are often associated with thermal inversions that trap pollutants near the ground surface\n
-            - High pressure is usually associated with stable air and lack of atmospheric gas mixing, resulting in pollutants accumulating\n
+    - Low temperatures are often associated with thermal inversions that trap pollutants near the ground surface\n
+    - High pressure is usually associated with stable air and lack of atmospheric gas mixing, resulting in pollutants accumulating\n
     
     **Patterns by Temperature Category (Advanced Analysis)**\n
     ****
     **Findings:**\n
-            - Cold Conditions (<0°C): Show the highest PM10 concentrations throughout the day, especially at night\n
-            - Cool Conditions (0-10°C): Show similar daily patterns but with lower concentrations\n
-            - Warm Conditions (>10°C): Shows significantly lower PM10 concentrations\n
+    - Cold Conditions (<0°C): Show the highest PM10 concentrations throughout the day, especially at night\n
+    - Cool Conditions (0-10°C): Show similar daily patterns but with lower concentrations\n
+    - Warm Conditions (>10°C): Shows significantly lower PM10 concentrations\n
     """)
 
     st.title("Recomendation related the Air Quality based on Analysis")
     st.info("""
     **Trafic**\n
     ****
-            - Encourage the use of public transportation to reduce the use of private vehicles\n
+    - Encourage the use of public transportation to reduce the use of private vehicles\n
     
     **Seasonality**\n
     ****
-            - Improve monitoring and enforcement of industrial emissions during December-March when PM10 concentrations are highest\n
-            - Develop an early warning system for air quality based on meteorological forecasts, especially during low temperature conditions\n
-            - Implement operational restrictions on emission sources (private vehicles, heaters, etc.) during periods of cold air with high pressure\n
+    - Improve monitoring and enforcement of industrial emissions during December-March when PM10 concentrations are highest\n
+    - Develop an early warning system for air quality based on meteorological forecasts, especially during low temperature conditions\n
+    - Implement operational restrictions on emission sources (private vehicles, heaters, etc.) during periods of cold air with high pressure\n
     
     **Energy Management**\n
     ****
-            - Introduce and support programs to switch from vehicles with carbon-based fuels to renewable energy\n
+    - Introduce and support programs to switch from vehicles with carbon-based fuels to renewable energy\n
             
     **Public Health Policy**\n
     ****  
-            - Raise public awareness about the risks of PM10 exposure, especially during low temperatures and high pressures\n
-            - Provide customized health recommendations for vulnerable groups (children, elderly, respiratory disease sufferers) based on identified daily and seasonal patterns\n
+    - Raise public awareness about the risks of PM10 exposure, especially during low temperatures and high pressures\n
+    - Provide customized health recommendations for vulnerable groups (children, elderly, respiratory disease sufferers) based on identified daily and seasonal patterns\n
     """)
